1f7a78c18a8b4e10a1e27626ac3df96b0e30d758
sophieklm <sklm@live.co.uk>
2017-04-03 14:04:12 +0100

    completes spec and passes all tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'return "fizz" when passed 3' do
+  it 'return "fizz" when passed a number divisible by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'return "buzz" when passed a number divisible by 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'return "fizzbuzz" when passed number divisible by 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns the given number when not divisible by 3 or 5' do
+    expect(fizzbuzz(4)).to eq 4
   end
 end
